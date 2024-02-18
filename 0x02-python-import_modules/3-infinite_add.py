017d3ee5b569ee969ff0b78730748926f8b39ba9
Wafaa HIJARY <wafaa.elhijary@gmail.com>
2024-02-18 23:12:52 +0100

    test

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Printt somme of all arguments yoo"""
     import sys
-
-    somme = 0
+    lasomme = 0
     for k in range(len(sys.argv) - 1):
-        somme += int(sys.argv[k + 1])
-    print("{}".format(somme))
+        lasomme += int(sys.argv[k + 1])
+    print("{}".format(lasomme))
