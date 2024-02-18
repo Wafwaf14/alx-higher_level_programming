ccd4c57597517215043fa4bb8f21be22687aa39d
Wafaa HIJARY <wafaa.elhijary@gmail.com>
2024-02-18 22:54:52 +0100

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """thiss is a program that prints nbr and  list of its arguments. """
     import sys
-    nbre_arguments = len(sys.argv) - 1
-    if nbre_arguments == 0:
+
+    count_num = len(sys.argv) - 1
+    if count_num == 0:
         print("0 arguments.")
-    elif nbre_arguments == 1:
+    elif count_num == 1:
         print("1 argument:")
-        print("1: {}".format(sys.argv[0]))
     else:
-        print("{} arguments:".format(nbre_arguments))
-        for k in range(nbre_arguments):
+        print("{} arguments:".format(count_num))
+        for k in range(count_num):
             print("{}: {}".format(k + 1, sys.argv[k + 1]))
